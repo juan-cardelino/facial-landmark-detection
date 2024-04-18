691ce5e65e3c357dcb33d2046a7b2c9ec0dd0583
hisokam324 <maurialmadab@gmail.com>
2024-04-18 12:16:36 -0300

    se agregaron formulas de calculos algebraicos

--- procesamiento.py
+++ procesamiento.py
@@ -15,13 +15,25 @@ def punto_recta(a, b, c):
 def producto_escalar(a, b):
     return sum(a*b)
 
-def perpendicular(v, u):
-    v1 = v/norma(v)
-    u1 = u - (sum(v1*u)*v1)
-    return v1, u1/norma(u1)
+#def perpendicular(v, u):
+#    v1 = v/norma(v)
+#    u1 = u - (sum(v1*u)*v1)
+#    return v1, u1/norma(u1)
+
+#def cambio(vector, eje_u):
+#    aux = np.array([[eje_u[0], eje_u[1]],[eje_u[1], -eje_u[0]]])
+#    aux2 = np.array([vector,]).T
+#    print(aux)
+#    print(vector)
+#    print(aux2)
+#   return aux*aux2.T
+
+def cambio(vector, eje_u):
+    aux = np.array([eje_u[1], -eje_u[0]])
+    return np.array([sum(vector*eje_u), sum(vector*aux)])
 
 def pr(u,v):
-    return sum(u*v)*v
+    return sum(u*v)
 
 def seno(alpha):
     return np.sin(np.arccos(alpha))
@@ -80,33 +92,39 @@ distfrente_ojo = punto_recta(centroideder, centroideizq, centrofrente)
 distboca_ojo = punto_recta(centroideder, centroideizq, centroboca)
 
 eje_ojos = np.abs(centroideder-centroideizq)
-eje_ojos, p_eje_ojos = perpendicular(eje_ojos, np.ones(2))
-print(eje_ojos)
-print(p_eje_ojos)
+eje_ojos = eje_ojos/norma(eje_ojos)
+p_eje_ojos = np.array([eje_ojos[1], -eje_ojos[0]])
+
 
 distfrente_ojo2 = pr(centrofrente, p_eje_ojos)
+distfrente_ojo21 = pr(centrofrente, eje_ojos)
 distboca_ojo2 = pr(centroboca, p_eje_ojos)
 origen_ojo1 = pr(centroideder, p_eje_ojos)
 origen_ojo2 = pr(centroideizq, p_eje_ojos)
 
+print("algo")
+print([distfrente_ojo21, distfrente_ojo2])
+print(cambio(centrofrente, eje_ojos))
+
 print('origen')
 print(origen_ojo1)
-print(norma(origen_ojo1))
+print(pr(origen_ojo, p_eje_ojos))
+#print(norma(origen_ojo1))
 print(centroideder)
 print(centroideizq)
 
 print('frente-boca')
 print(distfrente_ojo+distboca_ojo)
-print(norma(distboca_ojo2-distfrente_ojo2))
+#print(norma(distboca_ojo2-distfrente_ojo2))
 
 print('frente')
 print(distfrente_ojo)
-print(norma(distfrente_ojo2-origen_ojo1))
-print(norma(distfrente_ojo2)-norma(origen_ojo1))
+#print(norma(distfrente_ojo2-origen_ojo1))
+#print(norma(distfrente_ojo2)-norma(origen_ojo1))
 
 print("boca")
 print(distboca_ojo)
-print(norma(distboca_ojo2-origen_ojo1))
+#print(norma(distboca_ojo2-origen_ojo1))
 
 # idea que encontre en linea de como hace destancia de un punto a una recta https://es.stackoverflow.com/questions/62209/distancia-entre-punto-y-segmento
 #d = norm(np.cross(p2-p1, p1-p3))/norm(p2-p1)
@@ -118,11 +136,12 @@ cv2.circle(image, (int(centroideder[0]), int(centroideder[1])), 1, (0, 0, 255), 
 cv2.circle(image, (int(centroideizq[0]), int(centroideizq[1])), 1, (0, 0, 255), 5)
 cv2.circle(image, (int(centrofrente[0]), int(centrofrente[1])), 1, (0, 0, 255), 5)
 cv2.circle(image, (int(centroboca[0]), int(centroboca[1])), 1, (0, 0, 255), 5)
-cv2.circle(image, (int(origen_ojo1[0]), int(origen_ojo1[1])), 1, (0, 0, 255), 5)
-cv2.circle(image, (int(eje_ojos[0]), int(eje_ojos[1])), 1, (0, 0, 255), 5)
 cv2.circle(image, (int(origen_ojo[0]), int(origen_ojo[1])), 1, (0, 0, 255), 5)
-cv2.circle(image, (int(cejader[0][0]), int(cejader[0][1])), 1, (0, 255, 0), 5)
-cv2.circle(image, (int(cejader[-1][0]), int(cejader[-1][1])), 1, (0, 255, 0), 5)
+cv2.circle(image, (int(origen_ojo[0]+eje_ojos[0]*50), int(origen_ojo[1]+eje_ojos[1]*50)), 1, (0, 255, 0), 5)
+cv2.circle(image, (int(origen_ojo[0]+p_eje_ojos[0]*50), int(origen_ojo[1]+p_eje_ojos[1]*50)), 1, (0, 255, 0), 5)
+
+#cv2.circle(image, (int(cejader[0][0]), int(cejader[0][1])), 1, (0, 255, 0), 5)
+#cv2.circle(image, (int(cejader[-1][0]), int(cejader[-1][1])), 1, (0, 255, 0), 5)
 #for x, y in labioinf:
 #    cv2.circle(frame, (int(x), int(y)), 1, (0, 255, 0), 5)
 cv2.imwrite('face-processed.jpg', image)
@@ -149,5 +168,5 @@ data = {
 
 with open('data.json', 'w') as file:
     json.dump(data, file, indent=4)
-    
-algo = perpendicular(np.array([1,2]), np.array([1,2]))
+
+
