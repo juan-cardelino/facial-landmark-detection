48db99bb0982bd28e8edd2fd62808189c32687d2
hisokam324 <maurialmadab@gmail.com>
2024-05-01 10:35:55 -0300

    modificacion en presentacion y carga de marcadore

--- facial-landmark-from-file-modificada.py
+++ facial-landmark-from-file-modificada.py
@@ -17,8 +17,9 @@ def guardado(a_guardar):
     print('Marcadores guardados en formato json')       
     return
 
-verbose = 3
-minimo_ancho_de_cara = 50
+verbose = 4
+imagen = 3
+minimo_ancho_de_cara = 0
 
 input_dir = "input"
 detector = cv2.CascadeClassifier("data/haarcascade_frontalface_alt2.xml")
@@ -31,7 +32,6 @@ landmark_detector.loadModel("data/LFBmodel.yaml")
 
 #_, frame = webcam_cap.read()
 print('cargando archivo')
-imagen = 4
 input_fname = os.path.join(input_dir, str(os.listdir("input")[imagen]))
 frame = cv2.imread(input_fname)
 
@@ -47,48 +47,42 @@ print("caras detectadas: ",len(faces))
     
 #deteccion = {"caras":np.ones(len(faces)-1).tolist()}
 
-deteccion = {"caras":[], "cantidad de caras":len(faces), "mejor cara":{}, "Error":"No se encontraron errores"}
+deteccion = {"caras":[], "cantidad de caras":0, "mejor cara":{}, "Error":"No se encontraron errores"}
 iter = 0
 mejor_cara = [0, 0]
 for (x,y,w,d) in faces:
-    # Detect landmarks on "gray"
-    _, landmarks = landmark_detector.fit(gray, np.array(faces))
+    if verbose >= 4:
+        cv2.rectangle(frame, (x, y), (x+w, y+d), (255,255,255), int(w/64))
+    if w>minimo_ancho_de_cara:
+        # Detect landmarks on "gray"
+        _, landmarks = landmark_detector.fit(gray, np.array([[x, y, w, d]]))
     
-    lista = np.ones([68, 2])
-    landmarks = landmarks[:len(faces)]
-    #print(landmarks)
+        lista = landmarks[0][0]
     
-    for landmark in landmarks:
-        k = 0
-        for x,y in landmark[0]:
-            k = k + 1
-            # display landmarks on "frame/image,"
-            # with blue colour in BGR and thickness 2
-            #if k>=37 and k<=48:
-            if verbose >= 2:
+        if verbose >= 2:
+            for x, y in lista:
                 cv2.circle(frame, (int(x), int(y)), 1, (255, 0, 0), int(w/64))
-                #cv2.putText(frame, str(k) ,(int(x)+10, int(y)), cv2.FONT_HERSHEY_SIMPLEX , 1, (255,0,0), 1)
-            lista[k-1][0] = x
-            lista[k-1][1] = y
-            print(lista)
-    deteccion["caras"] = deteccion["caras"]+[1]
-    deteccion["caras"][iter] = {
-        "contorno":lista[0:17].tolist(),
-        "ceja derecha":lista[17:22].tolist(),
-        "ceja izquierda":lista[22:27].tolist(),
-        "tabique":lista[27:31].tolist(),
-        "fosas nasales":lista[31:36].tolist(),
-        "ojo derecho":lista[36:42].tolist(),
-        "ojo izquierdo":lista[42:48].tolist(),
-        "labio superior":lista[48:55].tolist()+lista[61:64].tolist(),
-        "labio inferior":lista[55:61].tolist()+lista[64:68].tolist()
-    }
-    if mejor_cara[1]<w:
-        mejor_cara = [iter, w]
-    iter = iter + 1
+                #cv2.putText(frame, str(k) ,(int(x)+10, int(y)), cv2.FONT_HERSHEY_SIMPLEX , 1, (255,0,0), 1)           
+   
+        deteccion["caras"] = deteccion["caras"]+[1]
+        deteccion["caras"][iter] = {
+            "ancho":int(w),
+            "contorno":lista[0:17].tolist(),
+            "ceja derecha":lista[17:22].tolist(),
+            "ceja izquierda":lista[22:27].tolist(),
+            "tabique":lista[27:31].tolist(),
+            "fosas nasales":lista[31:36].tolist(),
+            "ojo derecho":lista[36:42].tolist(),
+            "ojo izquierdo":lista[42:48].tolist(),
+            "labio superior":lista[48:55].tolist()+lista[61:64].tolist(),
+            "labio inferior":lista[55:61].tolist()+lista[64:68].tolist()
+        }
+        if mejor_cara[1]<w:
+            mejor_cara = [iter, w]
+        iter = iter + 1
 
-cv2.circle(frame, (faces[0][0], faces[0][1]), 1, (0, 255, 0), 10)
-cv2.circle(frame, (faces[0][0]+faces[0][2], faces[0][1]+faces[0][3]), 1, (0, 0, 255), 10)
+        
+
   
 if len(faces) != 0:
     deteccion["mejor cara"]["indice"]=int(mejor_cara[0])
