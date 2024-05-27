d3229d11243377f740dfa6cfecc8a088fdd49f0a
hisokam324 <maurialmadab@gmail.com>
2024-05-27 19:57:04 -0300

    sacar for extra en la deteccion de camara

--- camera_landmark.py
+++ camera_landmark.py
@@ -32,27 +32,29 @@ while(True):
     # Detect faces using the haarcascade classifier on the "grayscale image"
     faces = detector.detectMultiScale(gray)
     
-
-    for (x,y,w,d) in faces:
+    try:
         # Detect landmarks on "gray"
         _, landmarks = landmark_detector.fit(gray, np.array(faces))
         
         lista = landmarks[0]
-        print(len(landmarks))
-        if 1:
-            for i in landmarks:
-                centroideder=np.mean(i[0][36:42], axis=0)
-                #centroideder, centroideizq, unidad, origen_ojo, distojos, distfrente_ojo, distboca_ojo, angulo_cara, angulo_ojo_derecho, angulo_ojo_izquierdo, valores_elipse_ojoder, valores_elipse_ojoizq = pr.calculos(i[0][36:42], i[0][42:48], i[0][48:55], i[0][17:22])
-                #cv2.circle(frame, (int(centroideder[0]), int(centroideder[1])), 1, (0, 255, 0), 2)
-                #cv2.circle(frame, (int(centroideizq[0]), int(centroideizq[1])), 1, (0, 255, 0), 2)
-                #cv2.circle(frame, (int(origen_ojo[0]), int(origen_ojo[1])), 1, (0, 255, 0), 2)
-                #cv2.circle(frame, (int(centroideder[0]), int(centroideder[1])), 1, (0, 255, 0), 2)
+    except:
+        landmarks = []
+    print(len(landmarks))
+    if 1:
+        for i in landmarks:
+            centroideder=np.mean(i[0][36:42], axis=0)
+            #centroideder, centroideizq, unidad, origen_ojo, distojos, distfrente_ojo, distboca_ojo, angulo_cara, angulo_ojo_derecho, angulo_ojo_izquierdo, valores_elipse_ojoder, valores_elipse_ojoizq = pr.calculos(i[0][36:42], i[0][42:48], i[0][48:55], i[0][17:22])
+            #cv2.circle(frame, (int(centroideder[0]), int(centroideder[1])), 1, (0, 255, 0), 2)
+            #cv2.circle(frame, (int(centroideizq[0]), int(centroideizq[1])), 1, (0, 255, 0), 2)
+            #cv2.circle(frame, (int(origen_ojo[0]), int(origen_ojo[1])), 1, (0, 255, 0), 2)
+            #cv2.circle(frame, (int(centroideder[0]), int(centroideder[1])), 1, (0, 255, 0), 2)
 
-        for landmark in landmarks:
-            for x,y in landmark[0]:
-                # display landmarks on "frame/image,"
-                # with blue colour in BGR and thickness 2
-                cv2.circle(frame, (int(x), int(y)), 1, (255, 0, 0), 2)
+    for landmark in landmarks:
+        for x,y in landmark[0]:
+            # display landmarks on "frame/image,"
+            # with blue colour in BGR and thickness 2
+            cv2.circle(frame, (int(x), int(y)), 1, (255, 0, 0), 2)
+    
 
     # save last instance of detected image
     cv2.imwrite('face-detect.jpg', frame)    
