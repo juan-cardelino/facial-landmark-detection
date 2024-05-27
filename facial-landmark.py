8d128b85cd1e8c29940b7ab1f4d05139d8fac135
hisokam324 <maurialmadab@gmail.com>
2024-05-27 19:04:15 -0300

    ignorar face detect de camara

--- facial-landmark.py
+++ facial-landmark.py
@@ -12,64 +12,17 @@ import numpy as np
 #import procesamiento as pr
 
 
-# save face detection algorithm's url in haarcascade_url variable
-haarcascade_url = "https://raw.githubusercontent.com/opencv/opencv/master/data/haarcascades/haarcascade_frontalface_alt2.xml"
-
-# save face detection algorithm's name as haarcascade
-haarcascade = "haarcascade_frontalface_alt2.xml"
-haarcascade_clf = "data/" + haarcascade
-
-# check if data folder is in working directory
-if (os.path.isdir('data')):
-    # check if haarcascade is in data directory
-    if (haarcascade in os.listdir('data')):
-        print("File exists")
-    else:
-        # download file from url and save locally as haarcascade_frontalface_alt2.xml
-        urlreq.urlretrieve(haarcascade_url, haarcascade_clf)
-        print("File downloaded")
-else:
-    # create data folder in current directory
-    os.mkdir('data')
-    # download haarcascade to data folder
-    urlreq.urlretrieve(haarcascade_url, haarcascade_clf)
-    print("File downloaded")
-
 # create an instance of the Face Detection Cascade Classifier
-detector = cv2.CascadeClassifier(haarcascade_clf)
-
-# save facial landmark detection model's url in LBFmodel_url variable
-LBFmodel_url = "https://github.com/kurnianggoro/GSOC2017/raw/master/data/lbfmodel.yaml"
-
-# save facial landmark detection model's name as LBFmodel
-LBFmodel = "LFBmodel.yaml"
-LBFmodel_file = "data/" + LBFmodel
-
-# check if data folder is in working directory
-if (os.path.isdir('data')):
-    # check if Landmark detection model is in data directory
-    if (LBFmodel in os.listdir('data')):
-        print("File exists")
-    else:
-        # download file from url and save locally as haarcascade_frontalface_alt2.xml
-        urlreq.urlretrieve(LBFmodel_url, LBFmodel_file)
-        print("File downloaded")
-else:
-    # create data folder in current directory
-    os.mkdir('data')
-    # download Landmark detection model to data folder
-    urlreq.urlretrieve(LBFmodel_url, LBFmodel_file)
-    print("File downloaded")
+detector = cv2.CascadeClassifier("data/haarcascade_frontalface_alt2.xml")
 
 # create an instance of the Facial landmark Detector with the model
 landmark_detector  = cv2.face.createFacemarkLBF()
-landmark_detector.loadModel(LBFmodel_file)
+landmark_detector.loadModel("data/LFBmodel.yaml")
 
 # get image from webcam
 print ("checking webcam for connection ...")
 webcam_cap = cv2.VideoCapture(0)
 
-iter_p = 0
 
 while(True):
     # read webcam
@@ -96,16 +49,6 @@ while(True):
                 #cv2.circle(frame, (int(centroideizq[0]), int(centroideizq[1])), 1, (0, 255, 0), 2)
                 #cv2.circle(frame, (int(origen_ojo[0]), int(origen_ojo[1])), 1, (0, 255, 0), 2)
                 #cv2.circle(frame, (int(centroideder[0]), int(centroideder[1])), 1, (0, 255, 0), 2)
-        
-        lista_p = landmarks
-        if 0:
-            if iter_p == 0:
-                print(lista_p)
-                print(lista_p[0])
-                print(lista_p[0][0])
-                print(lista_p[0][0][0])
-                print(lista_p[0][0][0][0])
-                iter_p += 1
 
         for landmark in landmarks:
             for x,y in landmark[0]:
