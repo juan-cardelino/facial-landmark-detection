56a4836241deeecf86ff14a241affa0ec21789a7
hisokam324 <maurialmadab@gmail.com>
2024-06-19 17:08:33 -0300

    control headpose y integrar configuracion.json

--- control-database-binarizada.py
+++ control-database-binarizada.py
@@ -9,13 +9,15 @@ def probar_caracteristica(ffhq_data, detection_with_feature, data_with_feature, 
     actual = []
     predicted = []
     
-    print(len(data_with_feature))
-    
     for i in detection_with_feature:
         actual.append(not i in data_with_feature)
         predicted.append(not ffhq_data[i]['data'][caracteristica])
     
     confusion_matrix = metrics.confusion_matrix(predicted, actual)
+    tp = confusion_matrix[0][0]
+    fp = confusion_matrix[0][1]
+    fn = confusion_matrix[1][0]
+    tn = confusion_matrix[1][1]
 
     if verbose:
         cm_display = metrics.ConfusionMatrixDisplay(confusion_matrix = confusion_matrix, display_labels = ["True", "False"])
@@ -25,9 +27,32 @@ def probar_caracteristica(ffhq_data, detection_with_feature, data_with_feature, 
         plt.xlabel("Cara detectada")
         plt.show()
     
-    return
+    return [tp, fp, fn, tn]
 
-# Direccion de jsons
+def filtro(a, b, condicion=True):
+    aux_True = []
+    aux_False = []
+    for i in a:
+        if i in b:
+            aux_True.append(i)
+        else:
+            aux_False.append(i)
+    if condicion:
+        return aux_True
+    else:
+        return aux_False
+
+# Initial setup
+with open('configuracion.json') as file:
+    configuracion = json.load(file)
+
+json_suffix_detect = configuracion["path"]["json_suffix_detect"]
+json_suffix_data = configuracion["path"]["json_suffix_data"]
+json_dir = configuracion['path']['json_dir']
+dataset_binarizada_input = configuracion['pipeline']['binarizar dataset']['dataset_binarizada']
+verbose = configuracion['pipeline']['binarizar dataset']['verbose']
+
+# Aca reescribo porque si, en la version final borrarlo
 dir_json = 'FFHQ Json'
 
 # Lista de jsons
@@ -38,23 +63,27 @@ detection = []
 # Lista de imagenes con caras
 data = []
 
+l_json_suffix_detect = len(json_suffix_detect)+5
+l_json_suffix_data = len(json_suffix_data)+5
+
 # Identificar json procesados y caras detectadas
 for i in archivos_json:
     # Se identifica si el json es de deteccion
-    if i[-14:] == "deteccion.json":
-        detection.append(i[:-15])
+    if i[-l_json_suffix_detect:] == "{}.json".format(json_suffix_detect):
+        detection.append(i[:-l_json_suffix_detect-1])
     # Se identifica si el json es de data
-    elif i[-9:] == "data.json":
-        data.append(i[:-10])
+    elif i[-l_json_suffix_data:] == "{}.json".format(json_suffix_data):
+        data.append(i[:-l_json_suffix_data-1])
 
-# Se muestra la cantidad de jsons
-print("total json: {}".format(len(archivos_json)))
-# Se muestra la cantidad de imagenes procesadas
-print("deteccion: {}".format(len(detection)))
-# Se muestra la cantidad de imagenes con caras detectadas
-print("data: {}".format(len(data)))
-# Se muestra la cantidad de caras no detectadas y el porcentaje sobre el total
-print("{} caras no detectadas, {}%\n".format((len(detection)-len(data)), round(100*(len(detection)-len(data))/len(detection), 2)))
+if verbose:
+    # Se muestra la cantidad de jsons
+    print("total json: {}".format(len(archivos_json)))
+    # Se muestra la cantidad de imagenes procesadas
+    print("deteccion: {}".format(len(detection)))
+    # Se muestra la cantidad de imagenes con caras detectadas
+    print("data: {}".format(len(data)))
+    # Se muestra la cantidad de caras no detectadas y el porcentaje sobre el total
+    print("{} caras no detectadas, {}%\n".format((len(detection)-len(data)), round(100*(len(detection)-len(data))/len(detection), 2)))
 
 # Inicio bloque identificar archivos con caras no detectadas
 no_data = []
@@ -64,34 +93,40 @@ for i in detection:
         no_data.append(i)
 # Fin bloque identificar archivos con caras no detectadas
 
-with open('ffhq-feature-dataset-binarizado.json') as archivo:
+with open('{}.json'.format(dataset_binarizada_input)) as archivo:
         ffhq_data = json.load(archivo)
 
 no_existe_feature = ffhq_data['feature']['no existe']
 
-detection_with_feature = detection[:]
-for i in no_existe_feature:
-    if i in detection_with_feature:
-        detection_with_feature.remove(i)
+detection_with_feature = filtro(detection, no_existe_feature, False)
 
-data_with_feature = data[:]
-for i in data:
-    if not i in  detection_with_feature:
-        data_with_feature.remove(i)
+data_with_feature = filtro(data, detection_with_feature, condicion=True)
 
-print('data without features: {}, {}%'.format(len(data)-len(data_with_feature), round(100*(len(data)-len(data_with_feature))/len(data), 2)))
+if verbose:
+    print('data without features: {}, {}%'.format(len(data)-len(data_with_feature), round(100*(len(data)-len(data_with_feature))/len(data), 2)))
 
-no_data_with_feature = no_data[:]
-for i in no_data:
-    if not i in  detection_with_feature:
-        no_data_with_feature.remove(i)
-        
-print('no_data without features: {}, {}%'.format(len(no_data)-len(no_data_with_feature), round(100*(len(no_data)-len(no_data_with_feature))/len(no_data), 2)))
+no_data_with_feature = filtro(no_data, detection_with_feature, condicion=True)
 
-claves = list(ffhq_data["00055"]["data"].keys())
-print(claves)
-caracteristica = claves[6]
+if verbose:       
+    print('no_data without features: {}, {}%'.format(len(no_data)-len(no_data_with_feature), round(100*(len(no_data)-len(no_data_with_feature))/len(no_data), 2)))
 
-probar_caracteristica(ffhq_data, detection_with_feature, data_with_feature, caracteristica, True)
+# Extraer claves del 
+aux = list(ffhq_data.keys())
+claves = list(ffhq_data[aux[1]]["data"].keys())
 
-# sklearn o scipy-learn matriz de conjuncion 
+pruebas = []
+for clave in claves:
+    pruebas.append([clave, probar_caracteristica(ffhq_data, detection_with_feature, data_with_feature, clave, False)])
+
+for prueba in pruebas:
+    atributo = prueba[0]
+    tp = prueba[1][0]
+    fp = prueba[1][1]
+    fn = prueba[1][2]
+    tn = prueba[1][3]
+    accuracy = (tp+tn)/(tp+fp+fn+tn)
+    print('\n{}'.format(atributo.upper()))
+    print('True Positive: {}\nFalse Positive: {}\nFalse Negative: {}\nTrue Negative: {}'.format(tp, fp, fn, tn))
+    print("Accuracy: {}%".format(round(accuracy*100, 1)))
+
+print('Fin ejecucion')
--- reducir-y-binarizar-dataset.py
+++ reducir-y-binarizar-dataset.py
@@ -2,52 +2,102 @@ import os
 import json
 import numpy as np
 
+# Initial setup
+with open('configuracion.json') as file:
+    configuracion = json.load(file)
+
+FFHQ_path = configuracion['path']['input_dir']
+json_dir = configuracion['path']['json_dir']
+json_suffix_detect = configuracion['path']['json_suffix_detect']
+dataset_dir = configuracion['pipeline']['binarizar dataset']['dataset_dir']
+dataset_binarizada_output = configuracion['pipeline']['binarizar dataset']['dataset_binarizada']
+
+# Aca reescribo porque si, en la version final borrarlo
+FFHQ_path = 'FFHQ small'
+json_dir = 'FFHQ Json'
+
 # Lista de json generados de FFHQ
-archivos = os.listdir("FFHQ Json")
-# Direccion de las imagenes de FFHQ
-FFHQ_path = "C:/Users/mauri/OneDrive/Desktop/Facultad/Cuarto/Pasantia/FFHQsmall/faces_dataset_small/"
+archivos = os.listdir(json_dir)
 
 # Lista de las imagenes que fueron procesadas de FFHQ
 detection = []
 
 # Separar detecciones del resto de los archivos
+l_json_suffix_detect = len(json_suffix_detect)+5
 for i in archivos:
     # Se identifica si el json es de deteccion, si es se continua
-    if i[-14:] == "deteccion.json":
-        detection.append(i[:-15])
+    if i[-l_json_suffix_detect:] == '{}.json'.format(json_suffix_detect):
+        detection.append(i[:-l_json_suffix_detect-1])
 
 # Generar nuevo dataset con direccion de folder de fotos
 new_ffhq_data = {"FFHQ_path": FFHQ_path}
+# Lista de imagenes con feature
 existe_json = []
+# Lista de imagenes sin feature
 no_existe_json = []
 
+# Condiciones de pitch, roll y yaw
+pitch_condition = 20
+roll_condition = 12
+yaw_condition = 75
+
+# Control de atributos
+control_atributo = []
+
 for i in detection:
-    with open('FFHQ_json_features/{}.json'.format(i)) as dataset:
+    # Abrir el feature.json correspondiente a la imagen
+    with open('{}/{}.json'.format(dataset_dir, i)) as dataset:
         ffhq_data = json.load(dataset)
+    # Crear un atributo en el diccionario para guardar caracteristicas de la imagen
     new_ffhq_data[i] = {}
+    # Verificar si feature.json tiene informacion o no. Los feature.json so listas con diccionarios adentro
     if len(ffhq_data)>0:
-        new_ffhq_data[i]['error'] = 'no'
+        # Guardar que feature.json tiene informacion
+        new_ffhq_data[i]['error'] = False
+        # Generar atributo data para guardar la informacion relevante
         new_ffhq_data[i]['data'] = {}
+        
+        # Este bloque extrae las caracteristicas que son relevantes y la binariza
         new_ffhq_data[i]['data']['positove headpitch'] = ffhq_data[0]['faceAttributes']['headPose']['pitch'] > 0
         new_ffhq_data[i]['data']['positive headroll'] = ffhq_data[0]['faceAttributes']['headPose']['roll'] > 0
         new_ffhq_data[i]['data']['positive headyaw'] = ffhq_data[0]['faceAttributes']['headPose']['yaw'] > 0
-        new_ffhq_data[i]['data']['gender_male'] = ffhq_data[0]['faceAttributes']['gender'] == 'male'
-        new_ffhq_data[i]['data']['not glasses'] = not ffhq_data[0]['faceAttributes']['glasses'] == 'Glasses'
+        new_ffhq_data[i]['data']['centred headpitch'] = ffhq_data[0]['faceAttributes']['headPose']['pitch'] < pitch_condition and ffhq_data[0]['faceAttributes']['headPose']['pitch'] > -pitch_condition
+        new_ffhq_data[i]['data']['centred headroll'] = ffhq_data[0]['faceAttributes']['headPose']['roll'] < roll_condition and ffhq_data[0]['faceAttributes']['headPose']['roll'] > -roll_condition
+        new_ffhq_data[i]['data']['centred headyaw'] = ffhq_data[0]['faceAttributes']['headPose']['yaw'] < yaw_condition and ffhq_data[0]['faceAttributes']['headPose']['yaw'] > -yaw_condition
+        new_ffhq_data[i]['data']['centred face'] = new_ffhq_data[i]['data']['centred headpitch'] and new_ffhq_data[i]['data']['centred headroll'] and new_ffhq_data[i]['data']['centred headyaw']
+        new_ffhq_data[i]['data']['gender male'] = ffhq_data[0]['faceAttributes']['gender'] == 'male'
+        new_ffhq_data[i]['data']['not glasses'] = ffhq_data[0]['faceAttributes']['glasses'] == 'NoGlasses'
         new_ffhq_data[i]['data']['not blur'] = ffhq_data[0]['faceAttributes']['blur']['blurLevel'] == "low"
         new_ffhq_data[i]['data']['not noise'] = ffhq_data[0]['faceAttributes']['noise']['noiseLevel'] == "low"
         new_ffhq_data[i]['data']['forehead not occluded'] = not ffhq_data[0]['faceAttributes']['occlusion']['foreheadOccluded']
         new_ffhq_data[i]['data']['eye not occluded'] = not ffhq_data[0]['faceAttributes']['occlusion']['eyeOccluded']
         new_ffhq_data[i]['data']['mouth not occluded'] = not ffhq_data[0]['faceAttributes']['occlusion']['mouthOccluded']
+        # Fin bloque
+        
+        # Guardar imagen en lista de archivos con feature
         existe_json.append(i)
+        
+        # Este bloque existe con el fin de hacer pruebas
+        # Devuelve una lista con lo los posibles valores de un tributo en especifico de feature.json
+        atributo = ffhq_data[0]['faceAttributes']['headPose']['yaw']
+        if not atributo in control_atributo:
+            control_atributo.append(atributo)
+        # Fin bloque
     else:
-        new_ffhq_data[i]['error'] = 'si'
+        # Se guarda que feature.json no tiene informacion
+        new_ffhq_data[i]['error'] = True
+        # Guardar imagen en lista de archivos sin feature
         no_existe_json.append(i)
 
+# Se guarla la lista de archivos con y sin feature en el nuevo json
 new_ffhq_data['feature'] = {'existe':existe_json, 'no existe': no_existe_json}
 
-print(len(detection))
-print(len(new_ffhq_data)-1)
-
-with open('ffhq-feature-dataset-binarizado.json', 'w') as file:
+# Guardar dataset reducido y binarizado
+print('Generando dataset reducido y binarizado')
+with open('{}.json'.format(dataset_binarizada_output), 'w') as file:
     json.dump(new_ffhq_data, file, indent=4)
 
+if 0:
+    print(min(control_atributo), max(control_atributo))
+    
+print('Fin ejecucion')
