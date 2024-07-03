4f28a38a60e90c1b2a569f31fd038f848fe7fced
hisokam324 <maurialmadab@gmail.com>
2024-07-03 11:45:25 -0300

    sincronizar modulo alinear con readme

--- alinear.py
+++ alinear.py
@@ -1,25 +1,35 @@
 import json
 import cv2
 
-def get_json_data(imagen, json_dir, json_suffix):
-    with open(json_dir+'/'+imagen+'_'+json_suffix+'.json') as file:
+def get_json_data(image, json_dir, json_suffix):
+    # Open json file
+    with open('{}/{}_{}.json'.format(json_dir, image, json_suffix)) as file:
+        # Load json data
         data = json.load(file)
+    # Return image file, boundingbox and face angle
     return data['image file'], data['boundingbox'], data['angulos']['cara']
 
-def cropp(frame, boundingbox, porcentaje = 0):
+def cropp(frame, boundingbox, scale_boundingbox_by = 0):
+        # Get boundingbox corners coordenates
         x = boundingbox[0]
         y = boundingbox[1]
         w = boundingbox[2]
         d = boundingbox[3]
-    
-        start_x = x + int(w*porcentaje)
-        end_x = x + int(w*(1-porcentaje))
-        start_y = y + int(d*porcentaje)
-        end_y = y + int(d*(1-porcentaje))
+
+        # Get coordenates to cropp
+        start_x = x + int(w*scale_boundingbox_by)
+        end_x = x + int(w*(1-scale_boundingbox_by))
+        start_y = y + int(d*scale_boundingbox_by)
+        end_y = y + int(d*(1-scale_boundingbox_by))
+        # Return frame cropped
         return frame[start_y:end_y, start_x:end_x]
 
-def rotate(frame, boundingbox, angulo):
-        height, width = frame.shape[:2] 
+def rotate(frame, boundingbox, angle):
+        # Get frame height and width
+        height, width = frame.shape[:2]
+        # Get boundingbox center
         center = (boundingbox[0]+boundingbox[2]//2,boundingbox[1]+boundingbox[3]//2)
-        rotate_matrix = cv2.getRotationMatrix2D(center, angulo, 1)
+        # Ger rotation matrix
+        rotate_matrix = cv2.getRotationMatrix2D(center, angle, 1)
+        # Return frame rotated
         return cv2.warpAffine(frame, rotate_matrix, (width, height))
