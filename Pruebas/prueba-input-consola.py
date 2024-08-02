996da0b344a32194bf090bee2c51fda629d516f0
hisokam324 <108706867+hisokam324@users.noreply.github.com>
2024-08-02 18:29:08 -0300

    Delete Pruebas directory

--- Pruebas/prueba-input-consola.py
+++
@@ -1,8 +0,0 @@
-print("inserte primer numero a sumar: ")
-num1 = int(input())
-    
-print("inserte segundo numero a sumar: ")
-num2 = int(input())
- 
-# printing the sum in intager
-print(f"el resultado de la suma es: {num1 + num2}")
--- Pruebas/prueba_dir.py
+++
@@ -1,8 +0,0 @@
-import sys
-import os
-# Agregar la direccion fuera de la carpeta
-print(os.path.dirname(os.path.realpath(__file__)))
-parent_dir = os.path.abspath('..')
-print("dir")
-print(parent_dir)
-sys.path.append(parent_dir)
